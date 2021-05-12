48431d540361e485a4ded5a9aa7bc4f0bc3b0ff2
GitHub Action <action@github.com>
2021-05-12 14:16:42 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/derdan/openinassociatedapplication/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.derdan.openinassociatedapplication.services
+
+import com.github.derdan.openinassociatedapplication.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/derdan/openinassociatedapplication/services/MyProjectService.kt
+++ src/main/kotlin/com/github/derdan/openinassociatedapplication/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.derdan.openinassociatedapplication.services
 
+import com.github.derdan.openinassociatedapplication.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
