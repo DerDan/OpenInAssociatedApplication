79ddd8a9255ccc1a77d593dd3a6e2b420c13b935
Daniel Franke <franke@edf-online.de>
2023-05-11 23:25:27 +0200

    V1.0.7: - get a hint for non openable files - not longer "OpenInAssociatedApplication not available until indexer is   finished"

--- src/main/kotlin/com/github/derdan/openinassociatedapplication/OpenInAssociatedApplicationAction.kt
+++ src/main/kotlin/com/github/derdan/openinassociatedapplication/OpenInAssociatedApplicationAction.kt
@@ -4,17 +4,35 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.fileTypes.NativeFileType
+import com.intellij.openapi.project.DumbAware
+import javax.swing.JOptionPane
 
-class OpenInAssociatedApplicationAction : AnAction() {
-    override fun update(e: AnActionEvent) {
-        super.update(e)
-    }
+class OpenInAssociatedApplicationAction : AnAction(), DumbAware {
 
     override fun actionPerformed(anActionEvent: AnActionEvent) {
         val selectedFiles = anActionEvent.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY) ?: return
+        val notOpend: MutableList<String> = mutableListOf()
         if (selectedFiles.size < MAXIMAL_FILES) {
             for (file in selectedFiles)
-                NativeFileType.openAssociatedApplication(file)
+                if (file.isInLocalFileSystem) {
+                    NativeFileType.openAssociatedApplication(file)
+                } else {
+                    notOpend.add(file.presentableName)
+                }
+            if (notOpend.isNotEmpty()) {
+                JOptionPane.showMessageDialog(
+                    null,
+                    "file(s) could not be opened:\n${notOpend.joinToString(separator = "\n")}",
+                    "open in associated application",
+                    JOptionPane.ERROR_MESSAGE
+                )
+            }
+        } else {
+            JOptionPane.showMessageDialog(
+                null,
+                "to many files selected", "open in associated application", JOptionPane.ERROR_MESSAGE
+            )
+
         }
     }
 
